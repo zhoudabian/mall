9e23a5b8b03e9f2fae07aada2e90c1e19df5e434
zhoudabian <707614689@qq.com>
2020-04-14 17:40:55 +0800

    创建工程目录

---
+++ mall-member/src/test/java/com/frank/mall/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
